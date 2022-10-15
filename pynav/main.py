06224ffd65ba419a3bb298952001ab6b0bb1f2db
mxblsdl <maxblasdel@gmail.com>
2022-10-14 20:26:50 -0700

    add r function

--- pynav/main.py
+++ pynav/main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import typer
 import os
 import subprocess
@@ -5,6 +6,12 @@ from pathlib import Path
 from rich import print
 
 app = typer.Typer()
+
+# TODO move to other script
+def select_prompt(r, text):
+    [print(i, l) for i, l in enumerate(r)]
+    selection = typer.prompt(text)
+    return selection
 
 
 @app.command()
@@ -19,9 +26,8 @@ def go(path: str):
     search = list(filter(lambda l: path.lower() in l.lower(), lines))
 
     if len(search) > 1:
-        [print(i, l) for i, l in enumerate(search)]
-        selection = typer.prompt(
-            f"More than one matching path found\n Select desired path"
+        selection = select_prompt(
+            search, "More than one matching path found\n Select desired path"
         )
         out_path = search[int(selection)]
     else:
@@ -35,7 +41,7 @@ def go(path: str):
 
 @app.command()
 def add():  # add global flag here?
-    print("[bold red]Add paths to nav file [/bold red] :boom:")
+    print("[bold red]Add paths to nav file [/bold red] :emo:")
     print("[red]These paths can be accessed with `nav go -path`[/red]")
 
     # Get path of file being run
@@ -43,9 +49,54 @@ def add():  # add global flag here?
 
     if not Path.exists(config_file):
         Path(config_file).touch()
+        # TODO populate with template
 
     if "ix" in os.name:
         subprocess.run(["xdg-open", config_file])
     else:
         # os.system(f"Code {config_file}")
         os.startfile(config_file)
+
+
+@app.command("r")
+def open_r_proj(
+    proj: str = typer.Argument(""),
+    # list: Optional[str] = typer.Option(
+    #     None, help="Show R Project Files", rich_help_panel="Features"
+):
+
+    # Find projects
+    lines = (Path.home() / ".nav.conf").read_text().splitlines()
+    # Filter out comments
+    lines = [l for l in lines if not l.startswith("#")]
+    # Find appropraite index
+    idx = lines.index("[R Projects Folder]")
+    parent_dir = lines[idx + 1 : idx + 2][0]
+
+    # Find all projects
+    home = Path(parent_dir).expanduser()
+    rprojs = Path(home).rglob("*.Rproj")
+
+    # Show user projects
+    r_full = [r for r in rprojs]
+    r_full.sort()
+    r_name = [r.name for r in r_full]
+
+    if not proj == "":
+        r_tmp = list(filter(lambda r: proj.lower() in r.lower(), r_name))
+        # TODO add multi return logic
+        r_path = [r for r in r_full if str(r).endswith(r_tmp[0])]
+
+    else:
+        selection = select_prompt(r_name, "All R Projects")
+        # make selection
+        r_path = r_full[int(selection)]
+
+    # Launch project
+    print(r_path)
+    # subprocess.run(["xdg-open", r_path])
+
+
+# add()
+# open_r_proj("box")
+app()
